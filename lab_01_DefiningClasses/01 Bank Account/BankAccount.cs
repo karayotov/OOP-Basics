9427839f7aae666f88cae4181ebf33c8b47da089
karayotov <32219166+karayotov@users.noreply.github.com>
2018-02-13 18:15:39 +0200

    OOP firstLab

--- lab_01_DefiningClasses/01 Bank Account/BankAccount.cs
+++ lab_01_DefiningClasses/01 Bank Account/BankAccount.cs
@@ -1,5 +1,53 @@
-﻿class BankAccount
+﻿public class BankAccount
 {
-    private int id = 0;
-    private decimal balance = 0;
+    int id;
+    decimal balance;
+    
+    public int Id
+    {
+        get
+        {
+            return this.id;
+        }
+        set
+        {
+            this.id = value;
+        }
+    }
+
+    public decimal Balance
+    {
+        get { return this.balance; }
+        set { this.balance = value; }
+    }
+
+    public void Deposit(decimal amount)
+    {
+        this.Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (balance < amount)
+        {
+            System.Console.WriteLine("Insufficient balance");
+        }
+        else
+        {
+            this.Balance -= amount;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Account ID{this.Id}, balance {this.Balance:F2}";
+    }
+    private int myVar;
+
+    public int MyProperty
+    {
+        get { return myVar; }
+        set { myVar = value; }
+    }
+
 }
--- lab_01_DefiningClasses/01 Bank Account/mainClass.cs
+++ lab_01_DefiningClasses/01 Bank Account/mainClass.cs
@@ -1,9 +1,63 @@
-﻿class MainClass
+﻿using System;
+using System.Collections.Generic;
+
+public class MainClass
 {
     static void Main(string[] args)
     {
-        BankAccount acc = new BankAccount();
+        var accounts = new Dictionary<int, BankAccount>();
+        string command;
+        while ((command = Console.ReadLine()) != "End")
+        {
+            var splitCommand = command.Split();
+            var accountId = int.Parse(splitCommand[1]);
+            switch (splitCommand[0])
+            {
+                case "Create":
+                    if (accounts.ContainsKey(accountId))
+                    {
+                        Console.WriteLine($"Account already exist");
+                    }
+                    else
+                    {
+                        var account = new BankAccount();
+                        account.Id = accountId;
+                        accounts.Add(accountId, account);
+                    }
+                    break;
+                case "Deposit":
+                    if (ValidateAccountExists(accountId, accounts))
+                    {
+                        accounts[accountId].Deposit(int.Parse(splitCommand[2]));
+                    }
+                    break;
+                case "Withdraw":
+                    if(ValidateAccountExists(accountId, accounts))
+                    {
+                        accounts[accountId].Withdraw(int.Parse(splitCommand[2]));
+                    }
+                    break;
+                case "Print":
+                    if(ValidateAccountExists(accountId, accounts))
+                    {
+                        Console.WriteLine(accounts[accountId]);
+                    }
+                    break;
 
-        acc.Id = 1;
+            }
+        }
+    }
+
+    static bool ValidateAccountExists(int accountId, Dictionary<int, BankAccount> accounts)
+    {
+        if (accounts.ContainsKey(accountId))
+        {
+            return true;
+        }
+        else
+        {
+            Console.WriteLine($"Account does not exist");
+            return false;
+        }
     }
 }
