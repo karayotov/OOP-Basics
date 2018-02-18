4545707ffee6143232c375e6e153cacd276e6655
karayotov <32219166+karayotov@users.noreply.github.com>
2018-02-18 17:33:44 +0200

    01. CSharp-OOP-Basics-Defining-Classes-Exercises
    problem 07

---
+++ Exercise 01 - Defining Classes/SpeedRacing/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Car
+{
+    string model;
+    double fuelAmount;
+    double fuelConsumptionPerKilometer;
+    int distanceTraveled;
+    double howFarCanGo;
+
+    public string Model { get => model; set => model = value; }
+    public double FuelAmount { get => fuelAmount; set => fuelAmount = value; }
+    public double FuelConsumptionPerKilometer { get => fuelConsumptionPerKilometer; set => fuelConsumptionPerKilometer = value; }
+    public int DistanceTraveled { get => distanceTraveled; set => distanceTraveled = value; }
+    public double HowFarCanGo { get => howFarCanGo; set => howFarCanGo = value; }
+
+    public Car(int fuel, double consumptionPerKm)
+    {
+        FuelAmount = fuel;
+        FuelConsumptionPerKilometer = consumptionPerKm;
+        HowFarCanGo = FuelAmount / FuelConsumptionPerKilometer;
+        
+    }
+
+    public Car(string model, int fuel, double consumptionPerKm)
+        :this(fuel, consumptionPerKm)
+    {
+        Model = model;
+    }
+
+    public void HitTheRoad(int distance)
+    {
+        if (HowFarCanGo >= distance)
+        {
+            FuelAmount -= distance * FuelConsumptionPerKilometer;
+            DistanceTraveled += distance;
+            HowFarCanGo -= distance;
+        }
+        else
+        {
+            Console.WriteLine($"Insufficient fuel for the drive");
+        }
+    }
+
+    public void PrintCar(Car car)
+    {
+        Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.DistanceTraveled}");
+    }
+}
---
+++ Exercise 01 - Defining Classes/SpeedRacing/SpeedRacing.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRacing
+{
+    class SpeedRacing
+    {
+        static void Main(string[] args)
+        {
+            
+            int carsCount = int.Parse(Console.ReadLine());
+
+            List<Car> carList = AddCars(carsCount);
+
+            RaceTime(carList);
+            PrintCars(carList);
+
+        }
+
+        private static void PrintCars(List<Car> carList)
+        {
+            carList.ForEach(a => a.PrintCar(a));
+        }
+
+        private static void RaceTime(List<Car> carList)
+        {
+            string driveData;
+
+            while ((driveData = Console.ReadLine()) != "End")
+            {
+
+                string[] data = driveData.Split();
+                string model = data[1];
+                int distance = int.Parse(data[2]);
+
+                carList.Where(a => a.Model == model).ToList().ForEach(a => a.HitTheRoad(distance));
+            }
+        }
+
+        private static List<Car> AddCars(int carsCount)
+        {
+            List<Car> carList = new List<Car>();
+
+            for (int i = 0; i < carsCount; i++)
+            {
+
+                string[] carData = Console.ReadLine().Split();
+                Car car = new Car(carData[0], int.Parse(carData[1]), double.Parse(carData[2]));
+                carList.Add(car);
+            }
+
+            return carList;
+        }
+    }
+}
